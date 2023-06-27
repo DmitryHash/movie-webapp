991b61fb19ca77316fd80829ee9ae8a82d2844ca
DmitryHash <podolnitsky@gmail.com>
2023-06-27 10:02:28 +0300

    del search page, activate page, confirmReg page

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -2,16 +2,12 @@ import { FC } from "react";
 import { Route, Routes } from "react-router-dom";
 import { BlogPage } from "../pages/BlogPage/BlogPage";
 import { PostPage } from "../pages/PostPage/PostPage";
-import { SearchPage } from "../pages/SearchPage/SearchPage";
 import { SignInPage } from "../pages/SignInPage/SignInPage";
 import { SignUpPage } from "../pages/SignUpPage/SignUpPage";
-import { RegistrationConfirmPage } from "../pages/RegistrationConfirmPage/RegistrationConfirmPage";
-import { ActivatePage } from "../pages/ActivatePage/ActivatePage";
 import { useAppSelector } from "../store/hooks";
 import { ProtectedRoute } from "./ProtectedRoute";
 import { Settings } from "../pages/Settings/Settings";
 import { MovieInfo } from "../components/MainPageFilms/Movies/MovieInfo";
-import { Trends } from "../pages/TrendsPage/Trends";
 import { Favorites } from "../pages/FavoritesPage/Favorites";
 import { MovieListFilter } from '../../src/components/YearFilter/YearFilter';
 import { FilteredMoviesPage } from '../pages/FilteredMovieList/FilteredMoviePage';
@@ -29,19 +25,6 @@ export const Router: FC = () => {
 
             {/* <Route path='/main' element={<MainPage/>}/> */}
             <Route path="/settings" element={<Settings />} />
-            <Route
-                path="/trends"
-                element={
-                    <Trends
-                        handleFilterMovie={function (): void {
-                            throw new Error("Function not implemented.");
-                        }}
-                        handleMoveMain={function (): void {
-                            throw new Error("Function not implemented.");
-                        }}
-                    />
-                }
-            />
             <Route
                 path=""
                 element={
@@ -71,16 +54,11 @@ export const Router: FC = () => {
             />
             <Route path="/filtered-movies" element={<FilteredMoviesPage />}/>
             <Route path="/" element={<MovieListFilter />} />
-            <Route path="/search" element={<SearchPage />} />
             <Route path="/sign-in" element={<SignInPage />} />
             <Route path="/sign-up" element={<SignUpPage />} />
-            <Route path="/activate/:uid/:token" element={<ActivatePage />} />
 
             <Route element={<ProtectedRoute access={!!confirmEmail} />}>
-                <Route
-                    path="/confirm-registration"
-                    element={<RegistrationConfirmPage />}
-                />
+
             </Route>
 
             <Route path="*" element={<>Такой страницы не существует</>} />
