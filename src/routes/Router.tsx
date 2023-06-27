d0151cc1c21b9276ebb9a6025ebf7753e5c85ad7
DmitryHash <podolnitsky@gmail.com>
2023-06-27 10:15:27 +0300

    del post page

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,7 +1,6 @@
 import { FC } from "react";
 import { Route, Routes } from "react-router-dom";
 import { BlogPage } from "../pages/BlogPage/BlogPage";
-import { PostPage } from "../pages/PostPage/PostPage";
 import { SignInPage } from "../pages/SignInPage/SignInPage";
 import { SignUpPage } from "../pages/SignUpPage/SignUpPage";
 import { useAppSelector } from "../store/hooks";
@@ -38,7 +37,6 @@ export const Router: FC = () => {
                     />
                 }
             />
-            <Route path="/posts/:id" element={<PostPage />} />
 
             <Route
                 path="/movies/:id"
